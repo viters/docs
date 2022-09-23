7dded2cb8acf26a0e7e9f5858b7af9428a9558c6
Eron Powell <erondpowell@gmail.com>
2022-09-23 10:58:00 -0500

    tests

--- .vitepress/theme/Reference.vue
+++ .vitepress/theme/Reference.vue
@@ -1,16 +1,29 @@
 <script setup>
-// const x = {
-// 	"key": "value"
-// }
+import { ref } from 'vue'
 
-import json from './glossary.json';
-const glossary = json;
+function changeContent() {
+	console.log();
+}
+
+const apiTypes = ['REST', 'GraphQL', 'SDK'];
 
 </script>
 
-# Test
 <template>
-	<div>
-		<h1>{{glossary.key}}</h1>
+	<div class="button-container">
+		<button v-for="(apiType) in apiTypes" class="api-button" @click="changeContent">{{apiType}}</button>
 	</div>
 </template>
+
+<style scoped>
+.button-container {
+	border: solid;
+	border-radius: 8px;
+}
+
+.api-button {
+		padding: 10px;
+		margin: 5px;
+		background-color: red;
+	}
+</style>
