9614ba9805c30995e839b551db8c0eb60e7a5566
Eron Powell <erondpowell@gmail.com>
2022-10-07 12:44:17 -0500

    refactored funcs, renamed vars, fixed bug where pref didn't work on pageload, added todo notes

--- .vitepress/theme/Reference.vue
+++ .vitepress/theme/Reference.vue
@@ -1,17 +1,31 @@
 <script setup>
 	import { types } from './store.js';
 
-	const apiTypes = ['REST', 'GraphQL', 'SDK', 'CLI'];
+	const btnType = 'api';
+	const toggleOptions = ['REST', 'GraphQL', 'SDK', 'CLI'];
+	const defaultType = toggleOptions[0];
 
-	function changeContent(apiPref) {
-		types.setPreference(apiPref);
-		// console.log("types.pref:", types.pref);
+	function getPref() {
+		let usrPref = localStorage.getItem('apiType');
+		if (usrPref) {
+			types.pref = usrPref
+		} else {
+			types.pref = defaultType;
+		}
 	}
+
+	function setPref(apiPref) {
+		localStorage.setItem('apiType', apiPref);
+		types.pref = localStorage.getItem('apiType');
+		// console.log(localStorage.getItem('apiType'));
+	}
+
+	getPref();
 </script>
 
 <template>
 	<div class="api-button-container">
-		<button v-for="t in apiTypes" class="api-button" @click="changeContent(t)"> {{t}} </button>
+		<button v-for="t in toggleOptions" class="api-button" @click="setPref(t)"> {{t}} </button>
 		<slot :pref="types.pref"></slot>
 	</div>
 </template>
@@ -28,3 +42,18 @@
 			background-color: red;
 		}
 </style>
+
+
+<!--
+	TODO:
+	Add a "name/label" to the btn?? "The API" or "Skill Level" (added below)
+	DUse Obj name to assign btnType directly on the button in the  md file (e.g., :type="apiBtn")
+	Combine the type list and the types.pref into a single object, see below.
+
+	const apiBtn = {
+		btnType: "api",
+		btnLabel: "The API"
+		toggleOptions: ["REST", "GraphQL",  "SDK", "CLI"],
+		default: toggleOptions[0],
+	}
+-->
--- .vitepress/theme/store.js
+++ .vitepress/theme/store.js
@@ -1,15 +1,5 @@
 import { reactive } from 'vue';
 
 export const types = reactive({
-	pref: 'REST',
-
-	getPreference() {
-		return localStorage.getItem('apiType');
-	},
-
-	setPreference(apiPref) {
-		localStorage.setItem('apiType', apiPref);
-		this.pref = localStorage.getItem('apiType');
-		// console.log(localStorage.getItem('apiType'));
-	},
+	pref: '',
 });
