79367146baa4d8f049091794cb8e2f78e24acad7
Eron Powell <erondpowell@gmail.com>
2022-11-07 13:09:55 -0600

    started header component

---
+++ .vitepress/header/HeaderBlock.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+	<div class="header">
+		<slot name="title"></slot>
+		<slot name="tags"></slot>
+		<slot name="skill"></slot>
+		<slot name="version"></slot>
+		<slot name="author"></slot>
+
+	</div>
+</template>
+
+<style scoped>
+ .header {
+	padding: 2%;
+	background-color: var(--vp-c-brand-lighter);
+	border-style: solid;
+	border-width: 2px;
+	border-color: var(--vp-c-brand-light);
+	border-radius: 10px;
+ }
+</style>
--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,6 +1,7 @@
 import DefaultTheme from 'vitepress/theme';
 import Layout from './Layout.vue';
 import { createHead } from '@vueuse/head';
+import HeaderBlock from '../header/HeaderBlock.vue';
 
 import './vars.css';
 import './overrides.css';
@@ -10,6 +11,8 @@ export default {
 	...DefaultTheme,
 	Layout,
 	enhanceApp({ app }) {
+		// DefaultTheme.enhanceApp(ctx);
+		app.component('HeaderBlock', HeaderBlock);
 		const head = createHead();
 		app.use(head);
 	},
