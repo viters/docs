9357218fc0117ddc197d64566686340e0cd48499
Eron Powell <erondpowell@gmail.com>
2022-12-12 16:36:09 -0600

    moved

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -2,18 +2,18 @@ import DefaultTheme from 'vitepress/theme';
 import Layout from './Layout.vue';
 import { createHead } from '@vueuse/head';
 
+import SnippetToggler from '/components/SnippetToggler.vue';
+
 import './vars.css';
 import './overrides.css';
 import './icons.css';
-
-import { SnippetToggler } from '../components/SnippetToggler.vue';
 
 export default {
 	...DefaultTheme,
 	Layout,
 	enhanceApp({ app }) {
 		DefaultTheme.enhanceApp(ctx);
-		ctx.app.component('SnippetToggler', SnippetToggler);
+		// ctx.app.component('SnippetToggler', SnippetToggler);
 
 		const head = createHead();
 		app.use(head);
